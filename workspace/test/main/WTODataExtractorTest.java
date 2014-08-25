dc2d760561d5d5d730c2dce9fdfda46e2d5091f7
Denys Lazarenko <denys.lazarenko@gmail.com>
2014-08-25 11:31:48 +0200

    Organize project structure according to Eclipse workspace format. References libraries (POI, etc.) are now put into /workspace/libs folder.

---
+++ workspace/test/main/WTODataExtractorTest.java
@@ -0,0 +1,14 @@
+package main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WTODataExtractorTest {
+
+	@Test
+	public void testAnalyzeDocument() {
+		fail("Not yet implemented");
+	}
+
+}
